0db641736c8b8b2d110f62789290cb00cbe672d5
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-02-13 15:30:50 -0500

    Fix regex to grab voice names that have a space in them.

--- config/initializers/get_available_voices.rb
+++ config/initializers/get_available_voices.rb
@@ -1,6 +1,8 @@
 if system('which say')
   Rails.cache.write('cmd', 'say')
-  voices = `say -v ?`.split("\n").map {|line| line.split(" ").first }
+  voices = `say -v ?`.split("\n").map {|line|
+    line.split(/\s[a-z]/)[0].strip
+  }
   Rails.cache.write('voices', voices)
 else
   Rails.cache.write('cmd', 'whateverwepickforlinux')
